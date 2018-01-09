6495e360db399d3078da93c611ce63fc702ba120
IFinners <IDFinners@Gmail.com>
2018-01-09 13:44:51 +0000

    Organise Selenium section into goodreads_update function

--- booktracker.py
+++ booktracker.py
@@ -66,101 +66,114 @@ MONTH_CONV = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May',
                '11': 'Nov', '12': 'Dec'}
 
 # Selenium starts
-print('Updating Goodreads...')
-browser = webdriver.Firefox()
-browser.get('https://goodreads.com')
+def goodreads_update():
+    """Update Goodreads by marking book as read and filling in information."""
+    print('Updating Goodreads...')
+    browser = webdriver.Firefox()
+    browser.get('https://goodreads.com')
 
-# Login
-email_elem = browser.find_element_by_name('user[email]')
-email_elem.send_keys(USERNAME)
-pass_elem = browser.find_element_by_name('user[password]')
-pass_elem.send_keys(PASSWORD)
-pass_elem.send_keys(Keys.ENTER)
-time.sleep(5)
+    # Login
+    email_elem = browser.find_element_by_name('user[email]')
+    email_elem.send_keys(USERNAME)
+    pass_elem = browser.find_element_by_name('user[password]')
+    pass_elem.send_keys(PASSWORD)
+    pass_elem.send_keys(Keys.ENTER)
+    time.sleep(5)
 
-# Find correct book and edition
-SEARCH_TERMS = sys.argv[1]
-SEARCH_LIST = SEARCH_TERMS.split()       # UNUSED???
-search_elem = browser.find_element_by_class_name('searchBox__input')
-search_elem.send_keys(SEARCH_TERMS + '%3Dauthor')
-search_elem.send_keys(Keys.ENTER)
-time.sleep(3)
+    # Find correct book and edition
+    search_terms = sys.argv[1]
+    Search_list = search_terms.split()       # UNUSED???
+    search_elem = browser.find_element_by_class_name('searchBox__input')
+    search_elem.send_keys(search_terms + '%3Dauthor')
+    search_elem.send_keys(Keys.ENTER)
+    time.sleep(3)
 
-title_elem = browser.find_element_by_class_name('bookTitle')
-title_elem.click()
-time.sleep(3)
+    title_elem = browser.find_element_by_class_name('bookTitle')
+    title_elem.click()
+    time.sleep(3)
 
-editions_elem = browser.find_element_by_class_name('otherEditionsLink')
-editions_elem.click()
+    editions_elem = browser.find_element_by_class_name('otherEditionsLink')
+    editions_elem.click()
 
-# Format
-FORMAT = sys.argv[2]
-filter_elem = browser.find_element_by_name('filter_by_format')
-filter_elem.click()
-filter_elem.send_keys(FORMAT)
-filter_elem.send_keys(Keys.ENTER)
-time.sleep(3)
+    # Format
+    book_format = sys.argv[2]
+    filter_elem = browser.find_element_by_name('filter_by_format')
+    filter_elem.click()
+    filter_elem.send_keys(book_format)
+    filter_elem.send_keys(Keys.ENTER)
+    time.sleep(3)
 
-book_elem = browser.find_element_by_class_name('bookTitle')
-book_elem.click()
+    book_elem = browser.find_element_by_class_name('bookTitle')
+    book_elem.click()
 
-# Mark as Read
-menu_elem = browser.find_element_by_class_name('wtrRight.wtrUp')
-menu_elem.click()
-time.sleep(1)
-menu_elem.send_keys(Keys.TAB, Keys.TAB, Keys.TAB, Keys.ENTER)
-time.sleep(2)
+    # Mark as Read
+    menu_elem = browser.find_element_by_class_name('wtrRight.wtrUp')
+    menu_elem.click()
+    time.sleep(1)
+    menu_elem.send_keys(Keys.TAB, Keys.TAB, Keys.TAB, Keys.ENTER)
+    time.sleep(2)
 
-# Give star rating
-RATING = sys.argv[3]
-stars_elem = browser.find_elements_by_class_name('star.off')
-skip = 2
-for stars in stars_elem:
-    if stars.text.strip() == '{} of 5 stars'.format(RATING):
-        skip -= 1
-        if skip == 0:
+    # Date Selection
+    year_elem = browser.find_element_by_class_name('endedAtYear.readingSession'
+                                                   'DatePicker.smallPicker')
+    year_elem.click()
+    time.sleep(1)
+    year_elem.send_keys('2', Keys.ENTER)
+
+    month_elem = browser.find_element_by_class_name('endedAtMonth.largePicker'
+                                                    '.readingSessionDatePicker')
+    month_elem.click()
+    month_elem.send_keys(MONTH_CONV[DATE_READ[3:5]], Keys.ENTER)
+
+    day_elem = browser.find_element_by_class_name('endedAtDay.readingSession'
+                                                  'DatePicker.smallPicker')
+    day_elem.click()
+    day_elem.send_keys(str(DATE_READ[0:2]), Keys.ENTER)
+
+    # Save review
+    save_elem = browser.find_element_by_name('next')
+    save_elem.click()
+
+    # Shelf selection
+    shelves_elems = browser.find_elements_by_class_name('actionLinkLite.'
+                                                        'bookPageGenreLink')
+    shelves = []
+    for shelf in shelves_elems:
+        if ' users' not in shelf.text and shelf.text not in shelves:
+            shelves.append(shelf.text)
+
+    menu_elem = browser.find_element_by_class_name('wtrRight.wtrDown')
+    menu_elem.click()
+    time.sleep(1)
+    shelf_search_elem = browser.find_element_by_class_name('wtrShelfSearchField')
+
+    for i, item in enumerate(shelves):
+        shelf_search_elem.send_keys(shelves[i], Keys.ENTER)
+        shelf_search_elem.send_keys(Keys.SHIFT, Keys.HOME, Keys.DELETE)
+    
+    menu_elem.click()
+
+        # Give star rating
+    rating = sys.argv[3]
+    stars_elem = browser.find_elements_by_class_name('star.off')
+    for stars in stars_elem:
+        if stars.text.strip() == '{} of 5 stars'.format(rating):
             stars.click()
             break
 
+    print('Goodreads updated.')
 
-# Date Selection
-year_elem = browser.find_element_by_class_name('endedAtYear.readingSessionDatePicker.smallPicker')
-year_elem.click()
-time.sleep(1)
-year_elem.send_keys('2', Keys.ENTER)
+    shelves_and_url = []
+    shelves_and_url.append(shelves)
+    shelves_and_url.append(browser.current_url)
 
-month_elem = browser.find_element_by_class_name('endedAtMonth.largePicker.readingSessionDatePicker')
-month_elem.click()
-month_elem.send_keys(MONTH_CONV[DATE_READ[3:5]], Keys.ENTER)
+    return shelves_and_url
 
-day_elem = browser.find_element_by_class_name('endedAtDay.readingSessionDatePicker.smallPicker')
-day_elem.click()
-day_elem.send_keys(str(DATE_READ[0:2]), Keys.ENTER)
-
-# Save review
-save_elem = browser.find_element_by_name('next')
-save_elem.click()
-
-# Shelf selection
-shelves_elems = browser.find_elements_by_class_name('actionLinkLite.bookPageGenreLink')
-shelves = []
-for shelf in shelves_elems:
-    if ' users' not in shelf.text and shelf.text not in shelves:
-        shelves.append(shelf.text)
-
-menu_elem = browser.find_element_by_class_name('wtrRight.wtrDown')
-menu_elem.click()
-time.sleep(1)
-shelf_search_elem = browser.find_element_by_class_name('wtrShelfSearchField')
-
-for i, item in enumerate(shelves):
-    shelf_search_elem.send_keys(shelves[i], Keys.ENTER)
-    shelf_search_elem.send_keys(Keys.SHIFT, Keys.HOME, Keys.DELETE)
-
-print('Goodreads updated.')
+GOODREADS_INFO = goodreads_update()
+URL = GOODREADS_INFO[1]
+SHELVES_LIST = GOODREADS_INFO[0]
 
 # BS4 Parsing
-URL = browser.current_url
 RES = requests.get(URL)
 RES.raise_for_status()
 SOUP = bs4.BeautifulSoup(RES.text, 'html.parser')
@@ -227,7 +240,7 @@ WB.save(PATH)
 
 print('Spreadsheet has been updated.')
 
-# TODO Organise code into seperate functions
+# TODO Organise code better with functions
 # TODO Rewrite BS4 TAG system as Type and Genre can be found from the list 'Shelves'
-# TODO Rewrite title / series using seperaet getText's
+# TODO Rewrite title / series using seperate getText's
 # TODO Make constants and variables naming uniform and PEP8 compliant
