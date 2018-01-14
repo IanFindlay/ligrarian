e7dac1f5721c3d93dd2202c17c8f12e22e65ce40
IFinners <IDFinners@Gmail.com>
2018-01-14 17:34:27 +0000

    Rewrite get_date function to make use of strftime

--- booktracker.py
+++ booktracker.py
@@ -12,80 +12,58 @@ import openpyxl
 import bs4
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 
 def get_date():
     """Return the date the book was read formatted (DD/MM/YY)."""
     today = datetime.datetime.now()
 
-    date_day = today.day
-    if len(str(date_day)) == 1:
-        date_day = '0' + str(date_day)
-    date_month = today.month
-    if len(str(date_month)) == 1:
-        date_month = '0' + str(date_month)
-
-    date_year = str(today.year)[-2:]
-
     if len(sys.argv) == 4:
-        date = '{}/{}/{}'.format(date_day, date_month, date_year)
+        date = today.strftime('%d/%m/%y')
 
-    else:
-        date_read = sys.argv[4].lower()
+    elif sys.argv[4].lower() == 'y':
+        yesterday = today - timedelta(days=1)
+        date = yesterday.strftime('%d/%m/%y')
 
-        if date_read == 'y':
-            if date_day != 1:
-                date = '{}/{}/{}'.format('0' + str(int(date_day) - 1),
-                                         date_month, date_year)
-            else:
-                yesterday = datetime.datetime.now() - timedelta(days=1)
-
-                date_day = yesterday.day
-                date_month = yesterday.month
-                if len(str(date_month)) == 1:
-                    date_month = '0' + str(date_month)
-                date_year = str(yesterday.year)[-2:]
-
-                date = '{}/{}/{}'.format(date_day, date_month, date_year)
-
-        elif date_read == 'c':
-            date = input('Enter the date the book was finished (DD/MM/YY): ')
+    elif sys.argv[4].lower() == 'c':
+        date = input('Enter the date the book was finished (DD/MM/YY): ')
 
     return date
 
 
 def goodreads_find():
     """Find the correct book, in the correct format, on Goodreads."""
-    BROWSER.get('https://goodreads.com')
+    driver.get('https://goodreads.com')
 
     # Login
-    email_elem = BROWSER.find_element_by_name('user[email]')
-    email_elem.send_keys(USERNAME)
-    pass_elem = BROWSER.find_element_by_name('user[password]')
-    pass_elem.send_keys(PASSWORD)
+    email_elem = driver.find_element_by_name('user[email]')
+    email_elem.send_keys(username)
+    pass_elem = driver.find_element_by_name('user[password]')
+    pass_elem.send_keys(password)
     pass_elem.send_keys(Keys.ENTER)
 
     # Find correct book and edition
     search_terms = sys.argv[1]
-    search_elem = BROWSER.find_element_by_class_name('searchBox__input')
+    search_elem = driver.find_element_by_class_name('searchBox__input')
     search_elem.send_keys(search_terms + '%3Dauthor')
     search_elem.send_keys(Keys.ENTER)
 
-    BROWSER.find_element_by_class_name('bookTitle').click()
+    driver.find_element_by_class_name('bookTitle').click()
 
-    BROWSER.find_element_by_class_name('otherEditionsLink').click()
+    driver.find_element_by_class_name('otherEditionsLink').click()
 
     # Format
     book_format = sys.argv[2].lower()
-    filter_elem = BROWSER.find_element_by_name('filter_by_format')
+    filter_elem = driver.find_element_by_name('filter_by_format')
     filter_elem.click()
     filter_elem.send_keys(book_format)
     filter_elem.send_keys(Keys.ENTER)
     time.sleep(2)
 
-    BROWSER.find_element_by_class_name('bookTitle').click()
+    driver.find_element_by_class_name('bookTitle').click()
 
-    return BROWSER.current_url
+    return driver.current_url
 
 
 MONTH_CONV = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May',
@@ -96,43 +74,53 @@ MONTH_CONV = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May',
 def goodreads_update():
     """Update Goodreads by marking book as read and adding information."""
     # Mark as Read
-    menu_elem = BROWSER.find_element_by_class_name('wtrRight.wtrUp')
+    menu_elem = driver.find_element_by_class_name('wtrRight.wtrUp')
     menu_elem.click()
     time.sleep(1)
     menu_elem.send_keys(Keys.TAB, Keys.TAB, Keys.TAB, Keys.ENTER)
 
     # Date Selection
-    year_elem = BROWSER.find_element_by_class_name('endedAtYear.readingSession'
-                                                   'DatePicker.smallPicker')
-    year_elem.click()
-    year_elem.send_keys('2', Keys.ENTER)
+    # year_elem = driver.find_element_by_class_name('endedAtYear.readingSession'
+    #                                                'DatePicker.smallPicker')
+    # year_elem.click()
+    # year_elem.send_keys('2', Keys.ENTER)
 
-    month_elem = BROWSER.find_element_by_class_name('endedAtMonth.largePicker'
-                                                    '.readingSessionDatePicker')
-    month_elem.click()
-    month_elem.send_keys(MONTH_CONV[DATE_READ[3:5]], Keys.ENTER)
 
-    day_elem = BROWSER.find_element_by_class_name('endedAtDay.readingSession'
-                                                  'DatePicker.smallPicker')
-    day_elem.click()
-    day_elem.send_keys(str(DATE_READ[0:2]), Keys.ENTER)
+    # # FIXME Original implementation doesn't work for double digit days and
+    # # FIXME the new code is limited to only setting it to today's date
+    # day_elem = driver.find_element_by_class_name('endedAtDay.readingSession'
+    #                                               'DatePicker.smallPicker')
+    # day_elem.click()
+    # day_elem.send_keys(str(date_finished[0:2]))
+    # time.sleep(1)
+    # day_elem.send_keys(Keys.ENTER)
+
+
+    # month_elem = driver.find_element_by_class_name('endedAtMonth.largePicker'
+    #                                                 '.readingSessionDatePicker')
+    # month_elem.click()
+    # month_elem.send_keys(MONTH_CONV[date_finished[3:5]], Keys.ENTER)
+
+    # Set to today
+    today_elem = driver.find_element_by_class_name('endedAtSetTodayLink.gr-button')
+    today_elem.click()
 
     # Save review
-    save_elem = BROWSER.find_element_by_name('next')
+    save_elem = driver.find_element_by_name('next')
     save_elem.click()
 
     # Shelf selection
-    shelves_elems = BROWSER.find_elements_by_class_name('actionLinkLite.'
-                                                        'bookPageGenreLink')
+    shelves_elems = driver.find_elements_by_class_name('actionLinkLite.'
+                                                       'bookPageGenreLink')
     shelves = []
     for shelf in shelves_elems:
         if ' users' not in shelf.text and shelf.text not in shelves:
             shelves.append(shelf.text)
 
-    menu_elem = BROWSER.find_element_by_class_name('wtrRight.wtrDown')
+    menu_elem = driver.find_element_by_class_name('wtrRight.wtrDown')
     menu_elem.click()
     time.sleep(1)
-    shelf_search_elem = BROWSER.find_element_by_class_name('wtrShelfSearchField')
+    shelf_search_elem = driver.find_element_by_class_name('wtrShelfSearchField')
 
     for i in range(len(shelves)):
         shelf_search_elem.send_keys(shelves[i], Keys.ENTER)
@@ -142,11 +130,13 @@ def goodreads_update():
 
     # Give star rating
     rating = sys.argv[3]
-    stars_elem = BROWSER.find_elements_by_class_name('star.off')
+    stars_elem = driver.find_elements_by_class_name('star.off')
     for stars in stars_elem:
         if stars.text.strip() == '{} of 5 stars'.format(rating):
             stars.click()
             break
+
+    # TODO Add 5-star-books shelf to shelves if star rating is 5
 
     return shelves
 
@@ -154,7 +144,7 @@ def goodreads_update():
 def parse_page():
     """Parse and return page information needed for updating the spreadsheet."""
     info_list = []
-    res = requests.get(URL)
+    res = requests.get(url)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
@@ -176,12 +166,12 @@ def parse_page():
     pages = pages_elem[0].getText().split(',')[1].strip(' pages')
     info_list.append(pages)
 
-    if SHELVES_LIST[0] == 'Fiction' or SHELVES_LIST[0] == 'Nonfiction':
-        category = SHELVES_LIST[0]
-        genre = SHELVES_LIST[1]
+    if shelves_list[0] == 'Fiction' or shelves_list[0] == 'Nonfiction':
+        category = shelves_list[0]
+        genre = shelves_list[1]
     else:
-        genre = SHELVES_LIST[0]
-        if 'Fiction' in SHELVES_LIST:
+        genre = shelves_list[0]
+        if 'Fiction' in shelves_list:
             category = 'Fiction'
         else:
             category = 'Nonfiction'
@@ -194,7 +184,7 @@ def parse_page():
 
 def input_info(sheet_name):
     """Write the book information to the first blank row on the given sheet."""
-    sheet = WB.get_sheet_by_name(sheet_name)
+    sheet = wb.get_sheet_by_name(sheet_name)
     input_row = 1
     data = ''
     while data != None:
@@ -203,40 +193,40 @@ def input_info(sheet_name):
 
     input_row -= 1
 
-    sheet.cell(row=input_row, column=1).value = INFO[0]        # Title
-    sheet.cell(row=input_row, column=2).value = INFO[1]        # Author
-    sheet.cell(row=input_row, column=3).value = int(INFO[2])   # Pages
-    sheet.cell(row=input_row, column=4).value = INFO[3]        # Category
-    sheet.cell(row=input_row, column=5).value = INFO[4]        # Genre
-    sheet.cell(row=input_row, column=6).value = DATE_READ
+    sheet.cell(row=input_row, column=1).value = info[0]        # Title
+    sheet.cell(row=input_row, column=2).value = info[1]        # Author
+    sheet.cell(row=input_row, column=3).value = int(info[2])   # Pages
+    sheet.cell(row=input_row, column=4).value = info[3]        # Category
+    sheet.cell(row=input_row, column=5).value = info[4]        # Genre
+    sheet.cell(row=input_row, column=6).value = date_finished
 
 
-CONFIG = configparser.ConfigParser()
-CONFIG.read('/home/finners/Documents/Coding//Python/Booktracker/settings.ini')
-USERNAME = CONFIG.get('User', 'Username')
-PASSWORD = CONFIG.get('User', 'Password')
-PATH = CONFIG.get('Spreadsheet', 'Path')
+config = configparser.ConfigParser()
+config.read('/home/finners/Documents/Coding//Python/Booktracker/settings.ini')
+username = config.get('User', 'Username')
+password = config.get('User', 'Password')
+path = config.get('Spreadsheet', 'Path')
 
-DATE_READ = get_date()
+date_finished = get_date()
 
 print('Opening a computer controlled browser window and updating Goodreads...')
-BROWSER = webdriver.Firefox()
-BROWSER.implicitly_wait(5)
+driver = webdriver.Firefox()
+driver.implicitly_wait(5)
 
-URL = goodreads_find()
-SHELVES_LIST = goodreads_update()
-BROWSER.close()
+url = goodreads_find()
+shelves_list = goodreads_update()
+driver.close()
 print('Goodreads account updated.')
 
 
-WB = openpyxl.load_workbook(PATH)
+wb = openpyxl.load_workbook(path)
 print('Updating Spreadsheet...')
-INFO = parse_page()
+info = parse_page()
 
-input_info('20' + DATE_READ[-2:])
+input_info('20' + date_finished[-2:])
 input_info('Overall')
 
-WB.save(PATH)
+wb.save(path)
 
 print('Spreadsheet has been updated.')
 print('Booktracker has completed updating both the website and the spreadsheet'
