a4d5090f8898e0b4ea03b833dc181d0a54189f49
IFinners <IDFinners@Gmail.com>
2018-10-17 00:19:05 +0100

    Add tests for writing to spreasheet and assertions

--- test_run.py
+++ test_run.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-"""Do a test run of the Selenium automation that resets account afterwards."""
+"""Do a test run of the Ligrarian that resets everything afterwards."""
 
 from datetime import datetime as dt
+
+import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import ligrarian
+
 
 email = ligrarian.get_setting('User', 'Email')
 password = ligrarian.get_setting('User', 'Password')
@@ -21,20 +24,58 @@ driver.implicitly_wait(10)
 
 ligrarian.goodreads_login(driver, email, password)
 url = ligrarian.goodreads_find(driver, book_info['title'], book_info['author'],
-                          book_info['format'])
+                               book_info['format'])
+
+# Check book is correct format
+info_rows = driver.find_elements_by_class_name('row')
+row_text = [row.text for row in info_rows]
+assert 'Kindle' in ''.join(row_text), "Book is in incorrect format."
 
 shelves_list = ligrarian.goodreads_update(driver, book_info['date'],
-                                     book_info['review'], book_info['rating'])
-
-
-print('Goodreads account updated.')
+                                          book_info['review'],
+                                          book_info['rating'])
 
 undo_elem = driver.find_element_by_class_name('wtrStatusRead.wtrUnshelve')
+assert undo_elem, "Book wasn't marked as read."
+print("Book successfully marked as read.")
+# Reset Goodreads account
 undo_elem.click()
 alert_obj = driver.switch_to.alert
 alert_obj.accept()
 
-print('Goodreads account reset.')
 # Check book no longer marked as read before closing window
-driver.find_element_by_class_name('wtrToRead')
+unread_check = driver.find_element_by_class_name('wtrToRead')
+print('Goodreads account reset correctly.')
 driver.close()
+
+# Spreadsheet entry testing
+path = ligrarian.get_setting('Settings', 'Path')
+wb = openpyxl.load_workbook(path)
+print('Testing spreadsheet updating')
+info = ligrarian.parse_page(url, shelves_list)
+
+year_sheet = '20' + book_info['date'][-2:]
+
+# Get first blank row before attempting to write
+pre_year = ligrarian.first_blank(wb[year_sheet])
+pre_overall = ligrarian.first_blank(wb['Overall'])
+
+# Try to write info to both sheets
+ligrarian.input_info(wb, year_sheet, info['title'], info['author'],
+                     info['pages'], info['category'],
+                     info['genre'], book_info['date'])
+
+ligrarian.input_info(wb, 'Overall', info['title'], info['author'],
+                     info['pages'], info['category'],
+                     info['genre'], book_info['date'])
+
+# Find blank rows now that the data should have been entered
+post_year = ligrarian.first_blank(wb[year_sheet])
+post_overall = ligrarian.first_blank(wb['Overall'])
+
+# Confirm data was entered
+assert pre_year < post_year, "Data not written to year sheet."
+assert pre_overall < post_overall, "Data not written to overall sheet."
+print("Data was written to sheet successfully.")
+
+print("Test run complete.")
