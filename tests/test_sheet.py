cc0c8bbfd79059756fb1e8c4cd1a8a01661c0f18
Ian Findlay <code@findlayian.com>
2020-10-21 19:33:40 +0100

    Add create_sheet() tests

--- tests/test_sheet.py
+++ tests/test_sheet.py
@@ -57,3 +57,80 @@ class TestCheckYearSheetExists:
         assert returned_workbook == mock_workbook
 
 
+class TestCreateSheet:
+    """Copies then renames sheet, blanks all but first, sets date function."""
+
+    @mock.patch('ligrarian.first_blank_row', return_value=0)
+    @mock.patch('ligrarian.openpyxl')
+    def test_copies_sheet(self, mock_pyxl, mock_first):
+        """Should call copy_worksheet on workbook object."""
+        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+
+        mock_pyxl.copy_worksheet.assert_called_once()
+
+    @mock.patch('ligrarian.first_blank_row', return_value=0)
+    @mock.patch('ligrarian.openpyxl')
+    def test_names_sheet(self, mock_pyxl, mock_first):
+        """Should name sheet to new_sheet_name argument."""
+        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+
+        assert mock_pyxl.copy_worksheet.return_value.title == 'new'
+
+    @mock.patch('ligrarian.first_blank_row', return_value=2)
+    @mock.patch('ligrarian.openpyxl')
+    def test_first_row_cells_not_modified(self, mock_pyxl, mock_first):
+        """First row not accessed by cell call for .value modification.
+
+        Cells are blanked by setting their .value attributes to None. This
+        requires the cell to be accessed via a call with row and column args;
+        so no call to row 1 column 2 indicates that the first row hasn't been
+        set to None.
+
+        """
+        mock_sheet = mock_pyxl.copy_worksheet
+        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+        cell_calls = mock_sheet.return_value.cell.call_args_list
+
+        assert mock.call(row=1, column=2) not in cell_calls
+
+    @mock.patch('ligrarian.first_blank_row', return_value=2)
+    @mock.patch('ligrarian.openpyxl')
+    def test_other_row_cells_modified(self, mock_pyxl, mock_first):
+        """Other rows cell's value attributes changed to None."""
+        fake_two_one = FakeCell(2, 1)
+        mock_pyxl.copy_worksheet.return_value.cell.side_effect = [
+                fake_two_one,
+                FakeCell(2, 2),
+                FakeCell(2, 3),
+                FakeCell(2, 4),
+                FakeCell(2, 5),
+                FakeCell(2, 6),
+                FakeCell(5, 8),
+        ]
+        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+        assert fake_two_one.value == None
+
+    @mock.patch('ligrarian.first_blank_row', return_value=2)
+    @mock.patch('ligrarian.openpyxl')
+    def test_date_formula_written_to_last_cell(self, mock_pyxl, mock_first):
+        """Cell 5, 8 equal to date formula."""
+        fake_last_call = FakeCell(5, 8)
+        mock_pyxl.copy_worksheet.return_value.cell.side_effect = [
+                FakeCell(2, 1),
+                FakeCell(2, 2),
+                FakeCell(2, 3),
+                FakeCell(2, 4),
+                FakeCell(2, 5),
+                FakeCell(2, 6),
+                fake_last_call,
+        ]
+        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+        assert fake_last_call.value == "=(TODAY()-DATE(new,1,1))/7"
+
+
+
+
+
+
+
+
