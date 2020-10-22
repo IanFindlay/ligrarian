208925a27f0ee9ef32574de88e6950112a16df20
Ian Findlay <code@findlayian.com>
2020-10-22 14:05:10 +0100

    Refactor to patch classes where appropriate

--- tests/test_sheet.py
+++ tests/test_sheet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import pytest
+"""Tests for the openpyxl, spreadsheet related functions of ligrarian.py."""
+
 import unittest.mock as mock
 
 import ligrarian
@@ -10,11 +11,15 @@ class FakeCell:
     """A fake version of openpyxl's workbook.sheet.cell."""
 
     def __init__(self, row, column):
-        """Use row to simulate different values in different cells."""
-        if row == 3:
-            self.value = ''
-        else:
-            self.value = 'not blank'
+        self.row = row
+        self.column = column
+        self.value = "not blank"
+        self.set_value()
+
+    def set_value(self):
+        """Set self.value to None for cell(3, 1) to simulate blank row."""
+        if self.row == 3 and self.column == 1:
+            self.value = None
 
 
 def test_first_blank_row():
@@ -26,12 +31,12 @@ def test_first_blank_row():
     assert ligrarian.first_blank_row(mock_sheet) == 3
 
 
+@mock.patch('ligrarian.openpyxl')
 class TestCheckYearSheetExists:
     """create_sheet only if no year_sheet in workbook, return workbook."""
 
     @mock.patch('ligrarian.create_sheet')
-    @mock.patch('ligrarian.openpyxl')
-    def test_year_sheet_exists_no_create_call(self, mock_pyxl, mock_create):
+    def test_year_sheet_exists_no_create_call(self, mock_create, mock_pyxl):
         """No call to create_sheet."""
         mock_pyxl.load_workbook.return_value.sheetnames = ['2019', '2020']
         ligrarian.check_year_sheet_exists('', '2020')
@@ -39,15 +44,13 @@ class TestCheckYearSheetExists:
         mock_create.assert_not_called()
 
     @mock.patch('ligrarian.create_sheet')
-    @mock.patch('ligrarian.openpyxl')
-    def test_year_sheet_not_exist_create_call(self, mock_pyxl, mock_create):
+    def test_year_sheet_not_exist_create_call(self, mock_create, mock_pyxl):
         """One call to create_sheet."""
         mock_pyxl.load_workbook.return_value.sheetnames = ['2018', '2019']
         ligrarian.check_year_sheet_exists('', '2020')
 
         mock_create.assert_called_once()
 
-    @mock.patch('ligrarian.openpyxl')
     def test_workbook_returned(self, mock_pyxl):
         """Mocked workbook (load_workbook()) returned."""
         mock_workbook = mock_pyxl.load_workbook()
@@ -57,28 +60,24 @@ class TestCheckYearSheetExists:
         assert returned_workbook == mock_workbook
 
 
+@mock.patch('ligrarian.openpyxl')
+@mock.patch('ligrarian.first_blank_row', return_value=2)
 class TestCreateSheet:
     """Copies then renames sheet, blanks all but first, sets date function."""
 
-    @mock.patch('ligrarian.first_blank_row', return_value=0)
-    @mock.patch('ligrarian.openpyxl')
-    def test_copies_sheet(self, mock_pyxl, mock_first):
+    def test_copies_sheet(self, mock_first, mock_pyxl):
         """Should call copy_worksheet on workbook object."""
-        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+        ligrarian.create_sheet(ligrarian.openpyxl, 'copy', 'new')
 
-        mock_pyxl.copy_worksheet.assert_called_once()
+        ligrarian.openpyxl.copy_worksheet.assert_called_once()
 
-    @mock.patch('ligrarian.first_blank_row', return_value=0)
-    @mock.patch('ligrarian.openpyxl')
-    def test_names_sheet(self, mock_pyxl, mock_first):
+    def test_names_sheet(self, mock_first, mock_pyxl):
         """Should name sheet to new_sheet_name argument."""
-        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+        ligrarian.create_sheet(ligrarian.openpyxl, 'copy', 'new')
 
-        assert mock_pyxl.copy_worksheet.return_value.title == 'new'
+        assert ligrarian.openpyxl.copy_worksheet.return_value.title == 'new'
 
-    @mock.patch('ligrarian.first_blank_row', return_value=2)
-    @mock.patch('ligrarian.openpyxl')
-    def test_first_row_cells_not_modified(self, mock_pyxl, mock_first):
+    def test_first_row_cells_not_modified(self, mock_first, mock_pyxl):
         """First row not accessed by cell call for .value modification.
 
         Cells are blanked by setting their .value attributes to None. This
@@ -87,18 +86,16 @@ class TestCreateSheet:
         set to None.
 
         """
-        mock_sheet = mock_pyxl.copy_worksheet
-        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+        mock_sheet = ligrarian.openpyxl.copy_worksheet
+        ligrarian.create_sheet(ligrarian.openpyxl, 'copy', 'new')
         cell_calls = mock_sheet.return_value.cell.call_args_list
 
         assert mock.call(row=1, column=2) not in cell_calls
 
-    @mock.patch('ligrarian.first_blank_row', return_value=2)
-    @mock.patch('ligrarian.openpyxl')
-    def test_other_row_cells_modified(self, mock_pyxl, mock_first):
+    def test_other_row_cells_modified(self, mock_first, mock_pyxl):
         """Other rows cell's value attributes changed to None."""
         fake_two_one = FakeCell(2, 1)
-        mock_pyxl.copy_worksheet.return_value.cell.side_effect = [
+        ligrarian.openpyxl.copy_worksheet.return_value.cell.side_effect = [
                 fake_two_one,
                 FakeCell(2, 2),
                 FakeCell(2, 3),
@@ -107,15 +104,13 @@ class TestCreateSheet:
                 FakeCell(2, 6),
                 FakeCell(5, 8),
         ]
-        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
-        assert fake_two_one.value == None
+        ligrarian.create_sheet(ligrarian.openpyxl, 'copy', 'new')
+        assert fake_two_one.value is None
 
-    @mock.patch('ligrarian.first_blank_row', return_value=2)
-    @mock.patch('ligrarian.openpyxl')
-    def test_date_formula_written_to_last_cell(self, mock_pyxl, mock_first):
+    def test_date_formula_written_to_last_cell(self, mock_first, mock_pyxl):
         """Cell 5, 8 equal to date formula."""
         fake_last_call = FakeCell(5, 8)
-        mock_pyxl.copy_worksheet.return_value.cell.side_effect = [
+        ligrarian.openpyxl.copy_worksheet.return_value.cell.side_effect = [
                 FakeCell(2, 1),
                 FakeCell(2, 2),
                 FakeCell(2, 3),
@@ -124,13 +119,5 @@ class TestCreateSheet:
                 FakeCell(2, 6),
                 fake_last_call,
         ]
-        ligrarian.create_sheet(mock_pyxl, 'copy', 'new')
+        ligrarian.create_sheet(ligrarian.openpyxl, 'copy', 'new')
         assert fake_last_call.value == "=(TODAY()-DATE(new,1,1))/7"
-
-
-
-
-
-
-
-
