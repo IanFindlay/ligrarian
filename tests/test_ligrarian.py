ada15ba333830e2c4e0e3b48cfb8daa505cb0344
Ian Findlay <code@findlayian.com>
2020-10-22 14:59:49 +0100

    Refactor to patch classes where appropriate

--- tests/test_ligrarian.py
+++ tests/test_ligrarian.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-import pytest
+"""Tests the functions in ligrarian not related to GUI, sheet or goodreads."""
+
 import unittest.mock as mock
 
 import ligrarian
 
 
+@mock.patch('ligrarian.configparser.ConfigParser')
 class TestRetrieveSettings:
     """Test function opens .ini and retrieves and processes values right."""
 
-    @mock.patch('ligrarian.configparser.ConfigParser')
     def test_retrieve_reads(self, mock_parser):
         """Should read settings.ini."""
         ligrarian.retrieve_settings()
         mock_parser.return_value.read.assert_called_once_with('settings.ini')
 
-    @mock.patch('ligrarian.configparser.ConfigParser')
     def test_returns_string_values(self, mock_parser):
         """Should return a string value."""
         mock_parser.return_value.sections.return_value = ['test']
@@ -23,7 +23,6 @@ class TestRetrieveSettings:
         settings = ligrarian.retrieve_settings()
         assert settings == {'key': 'value'}
 
-    @mock.patch('ligrarian.configparser.ConfigParser')
     def test_returns_boolean_values(self, mock_parser):
         """Should return a boolean value if key is prompt or headless."""
         mock_parser.return_value.sections.return_value = ['test']
@@ -47,103 +46,102 @@ class TestGetDateString:
         assert ligrarian.get_date_str(yesterday=True) == formatted_date
 
 
+@mock.patch('ligrarian.input')
 class TestCheckAndPromptForEmailPassword:
     """Test function returns, prompts for and calls with the right values."""
 
-    @mock.patch('ligrarian.input')
+    def create_mock_settings(self, **kwargs):
+        """Modify copy of settings dictionary via kwargs then return it."""
+        default = {'email': 'email', 'password': None, 'prompt': True}
+        modified_settings = default.copy()
+        for setting, value in kwargs.items():
+            modified_settings[setting] = value
+        return modified_settings
+
     def test_no_email_input_if_set(self, mock_input):
         """If email set don't prompt for it."""
-        mock_settings = {'email': 'email', 'password': '', 'prompt': True}
+        mock_settings = self.create_mock_settings()
+        self.create_mock_settings(password='pass')
         ligrarian.check_and_prompt_for_email_password(mock_settings)
         assert mock.call('Email: ') not in mock_input.call_args_list
 
-    @mock.patch('ligrarian.input')
     def test_input_for_email_if_not_set(self, mock_input):
         """If email is not set then prompt for it."""
-        mock_settings = {'email': '', 'password': 'pass', 'prompt': True}
+        mock_settings = self.create_mock_settings(email=None, password='pass')
         ligrarian.check_and_prompt_for_email_password(mock_settings)
         assert mock.call('Email: ') in mock_input.call_args_list
 
-    @mock.patch('ligrarian.input')
     def test_inputted_email_saved_to_settings_dict(self, mock_input):
         """Inputted email saved to settings dict."""
-        mock_settings = {'email': '', 'password': 'pass', 'prompt': True}
+        mock_settings = self.create_mock_settings(email=None, password='pass')
         mock_input.return_value = 'inputted email'
         ligrarian.check_and_prompt_for_email_password(mock_settings)
         assert mock_settings['email'] == 'inputted email'
 
-    @mock.patch('ligrarian.input')
     def test_no_password_input_if_set(self, mock_input):
         """If password set don't prompt for it."""
-        mock_settings = {'email': '', 'password': 'pass', 'prompt': True}
+        mock_settings = self.create_mock_settings(password='pass')
         ligrarian.check_and_prompt_for_email_password(mock_settings)
         assert mock.call('Password: ') not in mock_input.call_args_list
 
-    @mock.patch('ligrarian.input')
     def test_password_input_if_set(self, mock_input):
         """If password is not set then prompt for it."""
-        mock_settings = {'email': 'email', 'password': '', 'prompt': True}
+        mock_settings = self.create_mock_settings()
         ligrarian.check_and_prompt_for_email_password(mock_settings)
         assert mock.call('Password: ') in mock_input.call_args_list
 
-    @mock.patch('ligrarian.input')
     def test_no_save_input_if_prompt_false(self, mock_input):
         """If password is not set and prompt False then no 'Save' input."""
-        mock_settings = {'email': '', 'password': '', 'prompt': False}
+        mock_settings = self.create_mock_settings(password=None, prompt=False)
         ligrarian.check_and_prompt_for_email_password(mock_settings)
         assert mock.call(
                 'Save Password?(y/n): ') not in mock_input.call_args_list
 
-    @mock.patch('ligrarian.input')
     def test_save_input_if_prompt_true(self, mock_input):
         """If password is not set and prompt True then 'Save' input."""
+        mock_settings = self.create_mock_settings()
         mock_settings = {'email': '', 'password': '', 'prompt': True}
         ligrarian.check_and_prompt_for_email_password(mock_settings)
         assert mock.call('Save Password?(y/n): ') in mock_input.call_args_list
 
-    @mock.patch('ligrarian.input')
     def test_password_saved_if_save_y(self, mock_input):
         """Password saved to settings dictionary if 'Save' 'y'."""
-        mock_settings = {'email': 'email', 'password': '', 'prompt': True}
+        mock_settings = self.create_mock_settings()
         mock_input.side_effect = ['inputted pass', 'y']
         ligrarian.check_and_prompt_for_email_password(mock_settings)
         assert mock_settings['password'] == 'inputted pass'
 
-    @mock.patch('ligrarian.input')
     def test_password_not_saved_if_save_n(self, mock_input):
         """Password not saved to settings dictionary if 'Save' 'n'."""
-        mock_settings = {'email': 'email', 'password': '', 'prompt': True}
+        mock_settings = self.create_mock_settings()
         mock_input.side_effect = ['inputted pass', 'n', 'n']
         ligrarian.check_and_prompt_for_email_password(mock_settings)
-        assert mock_settings['password'] == ''
+        assert mock_settings['password'] is None
 
-    @mock.patch('ligrarian.input')
     def test_prompt_set_to_true_if_disable_y(self, mock_input):
         """Prompt saved as False if disable y."""
-        mock_settings = {'email': 'email', 'password': '', 'prompt': True}
+        mock_settings = self.create_mock_settings()
         mock_input.side_effect = ['inputted pass', 'n', 'y']
         ligrarian.check_and_prompt_for_email_password(mock_settings)
-        assert mock_settings['prompt'] == False
+        assert mock_settings['prompt'] is False
 
-    @mock.patch('ligrarian.input')
     def test_prompt_set_to_false_if_disable_n(self, mock_input):
         """Prompt saved as True if disable n."""
-        mock_settings = {'email': 'email', 'password': '', 'prompt': True}
+        mock_settings = self.create_mock_settings()
         mock_input.side_effect = ['inputted pass', 'n', 'n']
         ligrarian.check_and_prompt_for_email_password(mock_settings)
-        assert mock_settings['prompt'] == True
+        assert mock_settings['prompt'] is True
 
 
+@mock.patch('ligrarian.open', new_callable=mock.mock_open())
 class TestWriteInitialConfig:
     """Test function writes config correctly and to the correct file."""
 
-    @mock.patch('ligrarian.open')
     def test_initial_config_opens_settings_file(self, mock_open):
         """Should open settings.ini in write mode."""
         ligrarian.write_initial_config()
         mock_open.assert_called_once_with('settings.ini', 'w')
 
-    @mock.patch('ligrarian.open', new_callable=mock.mock_open())
     def test_initial_config_write(self, mock_open_settings):
         """Should write default config to file.
 
@@ -156,17 +154,16 @@ class TestWriteInitialConfig:
         assert written_middle == "headless = False\n"
 
 
+@mock.patch('ligrarian.configparser.ConfigParser')
+@mock.patch('ligrarian.open')
 class TestWriteConfig:
     """Test function writes arguments to correct file."""
 
-    @mock.patch('ligrarian.open')
-    def test_write_config_opens_settings_file(self, mock_open):
+    def test_write_config_opens_settings_file(self, mock_open, mock_parser):
         """Should open settings.ini in write mode."""
         ligrarian.write_initial_config()
         mock_open.assert_called_once_with('settings.ini', 'w')
 
-    @mock.patch('ligrarian.configparser.ConfigParser')
-    @mock.patch('ligrarian.open')
     def test_write_config_email(self, mock_open, mock_parser):
         """ConfigParser.set should be called with argument email."""
         ligrarian.write_config("argument email", "password", "prompt")
@@ -174,8 +171,6 @@ class TestWriteConfig:
                 "user", "email", "argument email"
         )
 
-    @mock.patch('ligrarian.configparser.ConfigParser')
-    @mock.patch('ligrarian.open')
     def test_write_config_password(self, mock_open, mock_parser):
         """ConfigParser.set should be called with argument password."""
         ligrarian.write_config("email", "argument pass", "prompt")
@@ -183,8 +178,6 @@ class TestWriteConfig:
                 "user", "password", "argument pass"
         )
 
-    @mock.patch('ligrarian.configparser.ConfigParser')
-    @mock.patch('ligrarian.open')
     def test_write_config_prompt(self, mock_open, mock_parser):
         """ConfigParser.set should be called with argument prompt."""
         ligrarian.write_config("email", "password", "argument prompt")
