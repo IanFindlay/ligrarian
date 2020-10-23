481f3a3eb8c746a7fa8015513e40e430f8c1567a
Ian Findlay <code@findlayian.com>
2020-10-23 15:42:41 +0100

    Add docstrings to input_info() tests

--- tests/test_sheet.py
+++ tests/test_sheet.py
@@ -133,11 +133,11 @@ class TestInputInfo:
             'author': "mock author",
             'pages': "mock pages",
             'category': "mock category",
-            'genre': "mock genre",
+            'genre': "mock genre"
     }
 
     def mock_cell_access(self, sheet_name, test_value, test_index):
-        """."""
+        """Create FakeCell side_effect with one assigned."""
         test_value = FakeCell(9, 9)
         mock_cell = ligrarian.openpyxl[sheet_name].cell
         mock_side_effects = [FakeCell(1, 1)] * 12
@@ -147,7 +147,7 @@ class TestInputInfo:
         return (test_value, mock_cell.side_effect)
 
     def test_year_sheet_accessed(self, mock_first, mock_pyxl):
-        """."""
+        """2020 called on mock_pyxl.__getitem__."""
         ligrarian.input_info(mock_pyxl, mock.MagicMock(),
                              '2020', 'path')
         assert (
@@ -155,7 +155,7 @@ class TestInputInfo:
         )
 
     def test_overall_sheet_accessed(self, mock_first, mock_pyxl):
-        """."""
+        """Overall called on mock_pyxl.__getitem__."""
         ligrarian.input_info(
                 mock_pyxl, mock.MagicMock(),
                 '2020', 'path'
@@ -166,7 +166,7 @@ class TestInputInfo:
         )
 
     def test_accessed_year_sheet_title_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given title written to year sheet on index call."""
         fake_title, mock_sides = self.mock_cell_access('2020',
                                                         "mock title", 0)
         ligrarian.input_info(
@@ -175,106 +175,105 @@ class TestInputInfo:
         assert fake_title.value == "mock title"
 
     def test_accessed_year_sheet_author_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given author written to year sheet on index call."""
         fake_author, mock_sides = self.mock_cell_access('2020',
-                                                         "mock author", 1)
+                                                        "mock author", 1)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_author.value == "mock author"
 
     def test_accessed_year_sheet_pages_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given pages written to year sheet on index call."""
         fake_pages, mock_sides = self.mock_cell_access('2020',
-                                                        "mock pages", 2)
+                                                       "mock pages", 2)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_pages.value == "mock pages"
 
     def test_accessed_year_sheet_category_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given category written to year sheet on index call."""
         fake_category, mock_sides = self.mock_cell_access('2020',
-                                                        "mock category", 3)
+                                                          "mock category", 3)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_category.value == "mock category"
 
     def test_accessed_year_sheet_genre_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given genre written to year sheet on index call."""
         fake_genre, mock_sides = self.mock_cell_access('2020',
-                                                        "mock genre", 4)
+                                                       "mock genre", 4)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_genre.value == "mock genre"
 
     def test_accessed_year_sheet_date_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given date written to year sheet on index call."""
         fake_date, mock_sides = self.mock_cell_access('2020',
-                                                        "mock date", 5)
+                                                      "mock date", 5)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_date.value == "2020"
 
     def test_accessed_overall_sheet_title_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given title written to Overall sheet on index call."""
         fake_title, mock_sides = self.mock_cell_access('2020',
-                                                        "mock title", 6)
+                                                       "mock title", 6)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_title.value == "mock title"
 
     def test_accessed_overall_sheet_author_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given author written to Overall sheet on index call."""
         fake_author, mock_sides = self.mock_cell_access('2020',
-                                                         "mock author", 7)
+                                                        "mock author", 7)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_author.value == "mock author"
 
     def test_accessed_overall_sheet_pages_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given pages written to Overall sheet on index call."""
         fake_pages, mock_sides = self.mock_cell_access('2020',
-                                                        "mock pages", 8)
+                                                       "mock pages", 8)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_pages.value == "mock pages"
 
     def test_accessed_overall_sheet_category_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given category written to Overall sheet on index call."""
         fake_category, mock_sides = self.mock_cell_access('2020',
-                                                        "mock category", 9)
+                                                          "mock category", 9)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_category.value == "mock category"
 
     def test_accessed_overall_sheet_genre_set(self, mock_first, mock_pyxl):
-        """."""
+        """Given genre written to Overall sheet on index call."""
         fake_genre, mock_sides = self.mock_cell_access('2020',
-                                                        "mock genre", 10)
+                                                       "mock genre", 10)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_genre.value == "mock genre"
 
     def test_accessed_overall_sheet_date_set(self, mock_first, mock_pyxl):
-        """."""
-        fake_date, mock_sides = self.mock_cell_access('2020',
-                                                        "mock date", 11)
+        """Given date written to Overall sheet on index call."""
+        fake_date, mock_sides = self.mock_cell_access('2020', "mock date", 11)
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
         assert fake_date.value == "2020"
 
     def test_workbook_saved(self, mock_first, mock_pyxl):
-        """."""
+        """Save called on workbook (mock_pyxl)."""
         ligrarian.input_info(
                 mock_pyxl, self.mock_info, '2020', 'path')
 
